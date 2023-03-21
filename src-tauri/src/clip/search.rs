8003c207005635a3107ebe37cf357cf91c013d76
Zifan Hua <61620490+Alex222222222222@users.noreply.github.com>
2023-03-21 17:17:02 +0800

    add normal search
    Co-authored-by: Chloe1222 <keyiwangcw@gmail.com>

--- src-tauri/src/clip/search.rs
+++ src-tauri/src/clip/search.rs
@@ -26,6 +26,7 @@ pub fn fast_search(
     let clip_data = clip_data.clip_data.lock().unwrap();
     let mut statement = clip_data
         .database_connection
+        .as_ref()
         .unwrap()
         .prepare("SELECT * FROM clips WHERE id BETWEEN ? AND ? AND text MATCH ? LIMIT ?")
         .unwrap();
@@ -41,7 +42,102 @@ pub fn fast_search(
         .unwrap();
     let mut clips = Vec::new();
     loop {
-        let state = statement.next()
+        let state = statement.next();
+        match state{
+            Ok(State::Done) => {
+                break;
+            }
+            Ok(State::Row) => {
+                let id = statement.read::<i64,_>("id");
+                if let Err(err) = id  {
+                    return Err(error::Error::GetClipDataFromDatabaseErr(
+                        -1,
+                        err.message.unwrap(),
+                    ));
+                }
+                let id = id.unwrap();
+
+                let text = statement.read::<String,_>("text");
+                if let Err(err) = text  {
+                    return Err(error::Error::GetClipDataFromDatabaseErr(
+                        -1,
+                        err.message.unwrap(),
+                    ));
+                }
+                let text = text.unwrap();
+
+                let timestamp = statement.read::<i64,_>("timestamp");
+                if let Err(err) = timestamp  {
+                    return Err(error::Error::GetClipDataFromDatabaseErr(
+                        -1,
+                        err.message.unwrap(),
+                    ));
+                }
+                let timestamp = timestamp.unwrap();
+
+                let favorite = statement.read::<i64,_>("favorite");
+                if let Err(err) = favorite  {
+                    return Err(error::Error::GetClipDataFromDatabaseErr(
+                        -1,
+                        err.message.unwrap(),
+                    ));
+                }
+                let favorite = favorite.unwrap() == 1;
+
+                let clip = Clip {
+                    id,
+                    text,
+                    timestamp,
+                    favorite,
+                };
+                clips.push(clip);
+            }
+            Err(err) => {
+                return Err(error::Error::GetClipDataFromDatabaseErr(
+                    -1,
+                    err.message.unwrap(),
+                ));
+            }
+        }
+    }
+
+    Ok(clips)
+}
+
+/// normal search for a clip in the database
+/// SELECT count(*) FROM enrondata2 WHERE content LIKE '%linux%'
+/// will search for substring
+/// 
+/// this will try select clips match data wil id between min_id and max_id and maximum limit clips
+/// will return a list of clips
+pub fn normal_search(
+    app: &AppHandle,
+    min_id: i64,
+    max_id: i64,
+    limit: i64,
+    data: String,
+) -> Result<Vec<Clip>, error::Error> {
+    let clip_data = app.state::<ClipDataMutex>();
+    let clip_data = clip_data.clip_data.lock().unwrap();
+    let mut statement = clip_data
+        .database_connection
+        .as_ref()
+        .unwrap()
+        .prepare("SELECT * FROM clips WHERE id BETWEEN ? AND ? AND text Like ? LIMIT ?")
+        .unwrap();
+    statement
+        .bind::<&[(_, Value)]>(
+            &[
+                (1, min_id.into()),
+                (2, max_id.into()),
+                (3, ("%".to_string() + data.as_str() + "%").into()),
+                (4, limit.into()),
+            ][..],
+        )
+        .unwrap();
+    let mut clips = Vec::new();
+    loop {
+        let state = statement.next();
         match state{
             Ok(State::Done) => {
                 break;
