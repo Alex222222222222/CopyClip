460dfbf9ff085ce04b91195cd6206992c3cb8f63
Zifan Hua <61620490+Alex222222222222@users.noreply.github.com>
2023-03-25 20:03:04 +0800

    switch favorite icon

--- wasm-opt.sh
+++ wasm-opt.sh
@@ -8,19 +8,15 @@ if [ "$(uname)" != "Linux" ] && [ "$(uname)" != "Darwin" ]; then
 else
     echo "Optimizing wasm files..."
 
-    # install wasm-opt
-    if ! [ -x "$(command -v wasm-opt)" ]; then
-    cargo install wasm-opt
-    fi
-
     # list all wasm files
     wasm_files=$(find ./dist -name "*.wasm")
 
     # optimize wasm files
     for wasm_file in $wasm_files
     do
-        wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code $wasm_file -o $wasm_file annoying_space_waster
+        # wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code $wasm_file -o $wasm_file annoying_space_waster
         wasm-opt -Oz $wasm_file -o $wasm_file
+        wasm-opt -Os $wasm_file -o $wasm_file
     done
 
     twiggy top -n 20 ./dist/*.wasm
