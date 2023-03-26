ea6bb5d470def1a1bc369f08b2c2aa791add2e2e
Zifan Hua <61620490+Alex222222222222@users.noreply.github.com>
2023-03-26 15:51:14 +0800

    delete search clip per page config

--- src-tauri/src/backward/mod.rs
+++ src-tauri/src/backward/mod.rs
@@ -1,1 +1,2 @@
 pub mod v0_2_x_to_0_3_0_database;
+pub mod v0_3_x_to_0_3_3_config;
--- src/components/preferences/mod.rs
+++ src/components/preferences/mod.rs
@@ -1,7 +1,6 @@
 pub mod clips_per_page_config;
 pub mod log_level_filter_config;
 pub mod max_clip_len_config;
-pub mod search_clip_per_page;
 
 mod int_config_template;
 mod option_choose_config_template;
