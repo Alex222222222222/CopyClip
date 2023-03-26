fde279c84651f6c1c77275c905e378c20c119318
Chloe1222 <keyiwangcw@gmail.com>
2023-03-26 20:35:32 +0800

    add new todo

--- src/pages/preferences.rs
+++ src/pages/preferences.rs
@@ -3,15 +3,15 @@ use yew::{function_component, html, Html};
 use crate::components::{
     head_bar::HeadBar,
     preferences::{
-        clips_per_page_config::ClipsPerPageConfig, log_level_filter_config::LogLevelFilterConfig,
-        max_clip_len_config::MaxClipLenConfig, search_clip_per_page::SearchClipPerPageConfig,
+        clips_per_page_config::ClipsPerPageConfig, dark_mode_switch::DarkModeSwitch,
+        log_level_filter_config::LogLevelFilterConfig, max_clip_len_config::MaxClipLenConfig,
     },
 };
 
 #[function_component(Preferences)]
 pub fn preferences() -> Html {
     html! {
-        <div class="flex min-h-screen flex-col bg-white">
+        <div class="flex min-h-screen flex-col">
             <HeadBar></HeadBar>
             <h1 class="text-center text-6xl m-0">{ "Preferences" }</h1>
             <div class="mx-5 my-2">
@@ -19,9 +19,10 @@ pub fn preferences() -> Html {
                 <br />
                 <MaxClipLenConfig></MaxClipLenConfig>
                 <br />
-                <SearchClipPerPageConfig></SearchClipPerPageConfig>
-                <br />
                 <LogLevelFilterConfig></LogLevelFilterConfig>
+                <br />
+                // TODO add follow system theme
+                <DarkModeSwitch></DarkModeSwitch>
             </div>
         </div>
     }
