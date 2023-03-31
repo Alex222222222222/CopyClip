aef0b476461f1f72052730781f5403808c826c8e
Zifan Hua <huazifan@gmail.com>
2023-03-31 16:11:13 +0800

    change the state management to yewdux

--- src/components/preferences/dark_mode_switch.rs
+++ src/components/preferences/dark_mode_switch.rs
@@ -2,10 +2,7 @@ use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen_futures::spawn_local;
 use web_sys::{Event, HtmlInputElement};
-use yew::{
-    function_component, html, use_effect_with_deps, use_state, Callback, Html, Properties,
-    TargetCast, UseStateHandle,
-};
+use yew::{function_component, html, use_effect_with_deps, Callback, Html, TargetCast};
 
 use crate::invoke::invoke;
 
@@ -14,37 +11,30 @@ struct SetDataArg {
     data: bool,
 }
 
-#[derive(Clone, PartialEq, Properties)]
-struct SwitcherConfigTemplateProps {
-    pub label: String,
-    pub default_value: bool,
-    pub set_value_invoke: String,
-    pub get_value_invoke: String,
+#[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize, Eq, yewdux::prelude::Store)]
+#[store(storage = "local")]
+struct DarkModeState {
+    config: bool,
 }
 
 #[function_component(DarkModeSwitch)]
 pub fn dark_mode_switch() -> Html {
-    let props = SwitcherConfigTemplateProps {
-        label: "Dark Mode".to_string(),
-        default_value: false,
-        set_value_invoke: "set_dark_mode".to_string(),
-        get_value_invoke: "get_dark_mode".to_string(),
-    };
+    let label = "Dark Mode";
+    let set_value_invoke = "set_dark_mode";
+    let get_value_invoke = "get_dark_mode";
 
-    let value_handle: UseStateHandle<bool> = use_state(|| props.default_value);
-    let value = value_handle.clone();
+    let (dark_mode_state, dark_mode_dispatch) = yewdux::prelude::use_store::<DarkModeState>();
 
-    async fn handle_on_change(value: bool, set_value_invoke: String) {
+    async fn handle_on_change(value: bool, set_value_invoke: &str) {
         let args = to_value(&SetDataArg { data: value }).unwrap();
-        invoke(&set_value_invoke, args).await;
+        invoke(set_value_invoke, args).await;
     }
 
-    let label = props.label.clone();
-
-    let set_value_invoke = props.set_value_invoke.clone();
+    let dark_mode_dispatch_1 = dark_mode_dispatch.clone();
     let on_change = Callback::from(move |event: Event| {
         let value = event.target_unchecked_into::<HtmlInputElement>().checked();
-        let res = handle_on_change(value, set_value_invoke.clone());
+        let res = handle_on_change(value, set_value_invoke);
+        dark_mode_dispatch_1.set(DarkModeState { config: value });
         spawn_local(res);
 
         let window = web_sys::window().unwrap();
@@ -58,14 +48,13 @@ pub fn dark_mode_switch() -> Html {
         }
     });
 
-    let get_value_invoke = props.get_value_invoke.clone();
     use_effect_with_deps(
         move |_| {
             spawn_local(async move {
                 let args = to_value(&()).unwrap();
-                let res = invoke(&get_value_invoke, args).await;
+                let res = invoke(get_value_invoke, args).await;
                 let res = res.as_bool().unwrap();
-                value_handle.set(res);
+                dark_mode_dispatch.set(DarkModeState { config: res });
             });
         },
         (),
@@ -74,14 +63,14 @@ pub fn dark_mode_switch() -> Html {
     html! {
         <div class="flex flex-row justify-between">
             <label htmlFor="int-input-box" class="text-xl">
-                {label.clone()}
+                {label}
             </label>
             <label class="switch">
                 <input
                     type="checkbox"
                     id={format!("{}-input-box", label)}
                     onchange={on_change}
-                    checked={*value}
+                    checked={dark_mode_state.config}
                 />
                 <span class="slider dark:bg-white bg-black"></span>
             </label>
