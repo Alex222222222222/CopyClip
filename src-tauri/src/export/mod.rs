d30957186b0797bf33cd7e84865cf930815b458e
Zifan Hua <zifan.hua@icloud.com>
2023-04-18 13:27:03 +0800

    add cache maximum size, and expire time support

--- src-tauri/src/export/mod.rs
+++ src-tauri/src/export/mod.rs
@@ -112,7 +112,8 @@ async fn export_data(app: &AppHandle, path: String) -> Result<(), error::Error> 
     for i in 0..clip_data.clips.whole_list_of_ids.len() {
         let id = clip_data.clips.whole_list_of_ids[i];
         let clip = clip_data.get_clip(id).await?;
-        let c_json = serde_json::to_string(&clip);
+        let clip = clip.lock().unwrap();
+        let c_json = clip.to_json_string();
         if let Err(e) = c_json {
             return Err(error::Error::ExportError(e.to_string()));
         }
