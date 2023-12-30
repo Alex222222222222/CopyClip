38d104c442397adee6b3b44730fbaa490d8b91c8
Zifan Hua <zifan.hua@icloud.com>
2023-12-30 05:08:32 +0000

    Update Build Script
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- wasm-opt.sh
+++ wasm-opt.sh
@@ -6,17 +6,13 @@ if [ "$(uname)" != "Linux" ] && [ "$(uname)" != "Darwin" ]; then
   echo "This script only works on Linux or macOS"
 else
     echo "Optimizing wasm files..."
-
     # list all wasm files
     wasm_files=$(find ./dist -name "*.wasm")
-
     # optimize wasm files
-    for wasm_file in $wasm_files do
+    for wasm_file in $wasm_files
+    do
         echo "Optimizing $wasm_file..."
-        # wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code $wasm_file -o $wasm_file annoying_space_waster
         wasm-opt -Oz $wasm_file -o $wasm_file
         wasm-opt -Os $wasm_file -o $wasm_file
     done
-
-    #twiggy top -n 20 ./dist/*.wasm
 fi
