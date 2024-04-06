11b3d46b67f829d252f3c2f4546b4b7e97ab6a0a
Zifan Hua <zifan.hua@icloud.com>
2024-04-06 06:42:03 +0000

    Update rust-i18n from 2.3.1 to 3.0.1
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ extern crate rust_i18n;
 
 // Init translations for current crate.
 // You must keep this path is same as the path you set `load-path` in [package.metadata.i18n] in Cargo.toml.
-i18n!("locales");
+i18n!("locales", fallback = "en-GB");
 
 fn main() {
     yew::Renderer::<route::Main>::new().render();
