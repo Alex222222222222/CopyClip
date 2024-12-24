85e45eecb84e827be43383e1a5acbb4f49c134d2
Zifan Hua <zifan.hua@icloud.com>
2024-12-24 21:20:01 +0000

    Add eslint
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Search from "./pages/search";
+
+function Layout() {
+  return <></>;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="search" element={<Search />} />
+          {
+            // <Route index element={<Home />} />
+            // <Route path="blogs" element={<Blogs />} />
+            // <Route path="contact" element={<Contact />} />
+            // <Route path="*" element={<NoPage />} />
+          }
         </Route>
       </Routes>
     </BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), eslint()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
