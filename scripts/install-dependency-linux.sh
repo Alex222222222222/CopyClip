7e65d784f605b6cd12d5556a238d9e5d133910b0
Zifan Hua <zifan.hua@icloud.com>
2026-08-15 18:23:14 +0800

    chore: Update Linux dependency installation for consistency and clarity

--- scripts/install-dependency-linux.sh
+++ scripts/install-dependency-linux.sh
@@ -3,7 +3,8 @@ rustup component add rust-src clippy rustfmt
 cargo install tauri-cli
 cargo install trunk --locked
 sudo apt-get update
-sudo apt install libdbus-1-dev libwebkit2gtk-4.1-dev build-essential \
-    curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev \
-    librsvg2-dev xcb libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev \
-    libxcb-shape0-dev libxcb-xkb-dev libxcb-xfixes0-dev
+sudo apt-get install -y --no-install-recommends \
+    libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev \
+    libssl-dev libayatana-appindicator3-dev librsvg2-dev libdbus-1-dev \
+    libgtk-3-dev xcb libxcb-randr0-dev libxcb-xtest0-dev \
+    libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-xfixes0-dev
